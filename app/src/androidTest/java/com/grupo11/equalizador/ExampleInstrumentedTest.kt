c800db3c67fffb7bda5deacf64f1ca849ee1b156
Adriano Silva <asilv562@ford.com>
2025-05-08 21:37:24 -0300

    files_new

--- app/src/androidTest/java/com/grupo11/equalizador/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/grupo11/equalizador/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.grupo11.equalizador
+package com.grupo11.audioservice
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.grupo11.equalizador", appContext.packageName)
+        assertEquals("com.grupo11.audioservice", appContext.packageName)
     }
 }
